b2b8ed1dd2967aa1710b59938a36f6126a85ce9a
Julia Heidinger <juliaheidinger@gmx.net>
2018-12-03 13:03:57 +0100

    Center application form

--- src/components/ApplicationForm.js
+++ src/components/ApplicationForm.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Wrapper = styled.div`
+  /* max-width: 500px; */
   max-width: 500px;
-  width: 100%;
-  margin: 0;
+  margin: 0 auto;
   background: #fff;
   box-shadow: 0 8px 16px rgba(0, 40, 100, 0.4);
   border: px solid black;
